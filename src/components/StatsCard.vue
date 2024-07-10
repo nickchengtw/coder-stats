9d4e7215045183dcff4c97749e84dd1f74b12f17
nickchengtw <33611456+nickchengtw@users.noreply.github.com>
2024-07-10 19:12:48 +0800

    Upate StatsCard layout

--- src/components/StatsCard.vue
+++ src/components/StatsCard.vue
@@ -22,23 +22,23 @@ const props = defineProps({
 })
 
 const progressBars = ref([
-  {name: 'Easy', progress: 85},
-  {name: 'Medium', progress: 13},
-  {name: 'Hard', progress: 13},
+  { name: 'Easy', progress: 85 },
+  { name: 'Medium', progress: 13 },
+  { name: 'Hard', progress: 13 },
 ])
 const recentActivity = ref([
-  {date: new Date(Date.now()), submission: 5},
-  {date: new Date(Date.now()), submission: 5},
-  {date: new Date(Date.now()), submission: 5},
-  {date: new Date(Date.now()), submission: 5},
-  {date: new Date(Date.now()), submission: 5},
-  {date: new Date(Date.now()), submission: 5},
-  {date: new Date(Date.now()), submission: 5},
+  { date: new Date(Date.now()), submission: 5 },
+  { date: new Date(Date.now()), submission: 5 },
+  { date: new Date(Date.now()), submission: 5 },
+  { date: new Date(Date.now()), submission: 5 },
+  { date: new Date(Date.now()), submission: 5 },
+  { date: new Date(Date.now()), submission: 5 },
+  { date: new Date(Date.now()), submission: 5 },
 ])
 const recentProblems = ref([
-  {name: 'Two Sum', status: 'Accept'},
-  {name: 'Two Sum', status: 'Accept'},
-  {name: 'Two Sum', status: 'Accept'},
+  { name: 'Two Sum', status: 'Accept' },
+  { name: 'Two Sum', status: 'Accept' },
+  { name: 'Two Sum', status: 'Accept' },
 ])
 
 onMounted(async () => {
@@ -50,12 +50,12 @@ onMounted(async () => {
 
 <template>
   <Card class="mt-4 flex">
-    <div class="w-2/3">
+    <div class="w-1/3 flex flex-col">
       <CardHeader class="p-2 pt-0 md:p-4">
         <div class="flex items-center gap-4">
           <Avatar class="hidden h-9 w-9 sm:flex">
             <AvatarImage src="/avatars/03.png" alt="Avatar" />
-            <AvatarFallback>IN</AvatarFallback>
+            <AvatarFallback>?</AvatarFallback>
           </Avatar>
           <div class="grid gap-1">
             <p class="text-sm font-medium leading-none">
@@ -67,31 +67,33 @@ onMounted(async () => {
           </div>
         </div>
       </CardHeader>
-      <CardContent class="p-2 pt-0 md:p-4 md:pt-0 flex">
-        <div class="w-full">
-          <div class="flex items-center gap-4 mt-4" v-for="progress in progressBars">
-            <p class="text-sm min-w-16">{{ progress.name }}</p>
-            <Progress v-model="progress.progress" class="h-2" />
-          </div>
-        </div>
-        <div class="pl-8 pt-4 flex gap-2">
-          <div class="w-12 h-full flex flex-col" v-for="activity in recentActivity">
-            <p class="font-bold text-center mb-2">
+      <CardContent class="p-2 pt-0 md:p-4 md:pt-0 flex flex-grow">
+        <div class="flex flex-grow gap-2 justify-center items-end">
+          <div class="w-full flex flex-col item" v-for="(activity, idx) in recentActivity">
+            <p class="text-center mb-2" v-if="idx == 0 || idx == recentActivity.length - 1">
               {{ activity.date.getMonth() + 1 }}/{{ activity.date.getDate() }}
             </p>
-            <div class="h-full w-full bg-black rounded-lg"></div>
+            <div class="h-16 w-full bg-green-800 rounded-lg"></div>
           </div>
         </div>
       </CardContent>
     </div>
+
+    <div class="px-4 w-1/3 flex flex-col justify-center">
+      <div class="flex text-lg font-bold mx-auto">Solved : {{ 9999 }} / {{ 9999 }}</div>
+      <div class="flex items-center gap-4 mt-4" v-for="progress in progressBars">
+        <p class="text-sm min-w-16 font-bold">{{ progress.name }}</p>
+        <Progress v-model="progress.progress" class="h-2" />
+        <p class="text-sm min-w-20">{{ 1000 }} / {{ 1000 }}</p>
+      </div>
+    </div>
+
     <div class="pl-4 w-1/3">
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead>Recent problem</TableHead>
-            <TableHead class="text-right">
-              Status
-            </TableHead>
+            <TableHead>Recent AC</TableHead>
+            <TableHead class="text-right">Submission Time</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
